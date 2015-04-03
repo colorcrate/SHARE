fa153e65a4ce95cd5aefc798de9cc1f3b3ad2d5d
Eric Celeste <efc@clst.org>
2015-04-03 18:42:36 -0500

    Update wp-config.php
    remove credentials

--- wp-config.php
+++ wp-config.php
@@ -14,52 +14,10 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'share_wordpress');
-
-/** MySQL database username */
-define('DB_USER', 'wpuserwnkx081fsc');
-
-/** MySQL database password */
-define('DB_PASSWORD', 'Dy4GEa9oQg');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost:/tmp/mysql.sock');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
-/**#@+
- * Authentication Unique Keys and Salts.
- *
- * Change these to different unique phrases!
- * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
- * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
- * @since 2.6.0
+/*
+ * Change the following line to point to your private WordPress settings
  */
-define('AUTH_KEY',         'uo855o4kk9qtiq2iiyslnhwvw6g5ot4yogf5228rktis4xi0bragc73xbyiqq8kn');
-define('SECURE_AUTH_KEY',  'trcigxmamkfafu7n726mugi37rmmalidviumipyw9xrwd98shgf65rylsmvkk6r6');
-define('LOGGED_IN_KEY',    'j5687m6bpip49t6sbf3osxi2irqh1yu6t9phw4vy3tknvnywbcvlbkkfjl2grwww');
-define('NONCE_KEY',        '1pssq57lbul9lvc1w3073rm3qwpf6vhos25rvhtxf3g97omuwfqbhlklvp9jiwjm');
-define('AUTH_SALT',        'lqk66y2b9856s7pxkysls92ws5wkhvmqrjadxfj84jmi10p0cdchccwng73s4w0p');
-define('SECURE_AUTH_SALT', 'sqf7ylludoqgr9q14okt9knxj9vupk4j5p4ancln4lun5o1w8y4ccglwyl5jjcx3');
-define('LOGGED_IN_SALT',   '7v2oi0rqvd82q5nt86xc02i49omr3uhhxgc1q3xi5hcp6t9uxd8jy3rccjm80xo5');
-define('NONCE_SALT',       'tfty3ctxhtnp7cmpl2j507snjos0gxx814v8thb5p9wxxa2g8xx1qvciw7dj5l93');
-
-/**#@-*/
-
-/**
- * WordPress Database Table prefix.
- *
- * You can have multiple installations in one database if you give each a unique
- * prefix. Only numbers, letters, and underscores please!
- */
-$table_prefix  = 'wp_';
+require_once("/home/share/private/wp-config-private.php");
 
 /**
  * For developers: WordPress debugging mode.
